077513efdd41a34be5a3bd7a4e375b3710f43f43
ZZ22HohnholdJohannes <Johannes.Hohnhold@bs-erlangen.de>
2025-02-04 07:54:34 +0100

    Solution created

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Reserve_iT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
