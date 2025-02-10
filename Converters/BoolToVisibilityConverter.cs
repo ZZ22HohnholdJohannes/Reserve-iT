817f7a293cf1179b1d3bdd87e02720a7d67a35b0
ZZ22HohnholdJohannes <Johannes.Hohnhold@bs-erlangen.de>
2025-02-10 08:48:28 +0100

    Admin Login added

---
+++ Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Reserve_iT.Converters
+{
+  public class BoolToVisibilityConverter : IValueConverter
+  {
+    // Wenn der Parameter "invert" übergeben wird, wird das Ergebnis umgekehrt.
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      bool flag = false;
+      if (value is bool)
+        flag = (bool)value;
+
+      if (parameter != null && parameter.ToString() == "invert")
+        flag = !flag;
+
+      return flag ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (value is Visibility vis)
+      {
+        bool result = (vis == Visibility.Visible);
+        if (parameter != null && parameter.ToString() == "invert")
+          result = !result;
+        return result;
+      }
+      return false;
+    }
+  }
+}
--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -4,46 +4,80 @@ using Reserve_iT.View;
 
 namespace Reserve_iT.ViewModel
 {
-	public class MainWindowViewModel : NotifyObject
-	{
-		private object _currentView;
-		public object CurrentView
-		{
-			get => _currentView;
-			set
-			{
-				if (_currentView != value)
-				{
-					_currentView = value;
-					OnPropertyChanged();
-				}
-			}
-		}
+  public class MainWindowViewModel : NotifyObject
+  {
+    #region Properties
+    private object _currentView;
+    public object CurrentView
+    {
+      get => _currentView;
+      set
+      {
+        if (_currentView != value)
+        {
+          _currentView = value;
+          OnPropertyChanged();
+        }
+      }
+    }
+    public bool IsAdminLoggedIn
+    {
+      get => Get<bool>(nameof(IsAdminLoggedIn));
+      set => Set(value, nameof(IsAdminLoggedIn));
+    }
 
-		public ICommand ShowBookingSearchViewCommand { get; }
+    public string AdminPassword
+    {
+      get => Get<string>(nameof(AdminPassword));
+      set => Set(value, nameof(AdminPassword));
+    }
 
-		public ICommand GoToDashboardCommand { get; }
+    #endregion
 
-		public MainWindowViewModel()
-		{
-			// DashboardView to BookingSearchview
-			CurrentView = new DashboardView();
-			ShowBookingSearchViewCommand = new RelayCommand(ShowBookingSearchView);
+    #region Commands
+    public ICommand ShowBookingSearchViewCommand { get; }
+    public ICommand GoToDashboardCommand { get; }
+    public ICommand LoginCommand { get; }
+    #endregion
 
-			// BookingSearchView to DashboardView
-			CurrentView = new DashboardView();
-			GoToDashboardCommand = new RelayCommand(GoToDashboard);
-		}
+    #region Constructor
+    public MainWindowViewModel()
+    {
+      // Standardmäßig ist der Administrator-Status deaktiviert
+      IsAdminLoggedIn = false;
 
-		private void ShowBookingSearchView()
-		{
-			CurrentView = new BookingSearchView();
-		}
+      // Setze initial die DashboardView als Startansicht
+      CurrentView = new DashboardView();
 
-		private void GoToDashboard()
-		{
-			CurrentView = new DashboardView();
-		}
+      // Initialisiere die Commands
+      ShowBookingSearchViewCommand = new RelayCommand(ShowBookingSearchView);
+      GoToDashboardCommand = new RelayCommand(GoToDashboard);
+      LoginCommand = new RelayCommand(Login);
+    }
+    #endregion
 
-	}
+    #region Methods
+    private void ShowBookingSearchView()
+    {
+      CurrentView = new BookingSearchView();
+    }
+
+    private void GoToDashboard()
+    {
+      CurrentView = new DashboardView();
+    }
+
+    private void Login()
+    {
+      if (AdminPassword == "Administrator")
+      {
+        IsAdminLoggedIn = true;
+      }
+      else
+      {
+        IsAdminLoggedIn = false;
+      }
+    }
+    #endregion
+  }
 }
